7a719b1d2b76c1ddacf95d7228e0de9e39bdffe8
Raphael Mello <123605158+Raphaelpmros@users.noreply.github.com>
2024-04-22 22:14:40 -0300

    Projeto de pokedex da matéria de front end

---
+++ pokedex/src/components/ListPokemon.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const pokemon = defineProps(["name", "url"])
+    const pokemonId = pokemon.url.split('/')[6]
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`
+</script>
+
+<template>
+    <div class="col-md-4">
+        <div class="card p-2 mb-3">
+            <img :src="imageUrl" height="80" alt="">
+            <p class="text-center">{{ pokemon.name }}</p>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
